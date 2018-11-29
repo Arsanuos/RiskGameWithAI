716f06b752c56adc11f46527daf3a2c9f0c73f86
Amr Hendy <amr.m.hendy@gmail.com>
2018-11-29 19:51:57 +0200

    Added Astar and Greedy Agents

---
+++ riskGame/classes/agent/aStar_agent.py
@@ -0,0 +1,15 @@
+from riskGame.classes.agent.one_time_agent import OneTimeAgent
+from riskGame.classes.evaluations.sigmoidEval import SigmoidEval
+
+
+class AStar(OneTimeAgent):
+
+    def __init__(self, evaluation_heuristic=SigmoidEval):
+        self.__evaluation_heuristic = evaluation_heuristic
+        super(AStar).__init__(self.calculate_fn)
+
+    def calculate_fn(self, state):
+        gn = state.calculate_gn(self.__initial_state)
+        hn = self.__evaluation_heuristic(state).score()
+        return gn + hn
+
--- riskGame/classes/agent/agent.py
+++ riskGame/classes/agent/agent.py
@@ -1,4 +1,5 @@
 from riskGame.classes.state.move import Move
+from copy import deepcopy
 
 
 class Agent:
@@ -9,7 +10,8 @@ class Agent:
         self._attack_heuristic = attack_heuristic
 
     # move is None in all agents unless in case of human it will be not None
-    def play(self, state, move=None):
+    def play(self, current_state, move=None):
+        state = deepcopy(current_state)
         if move is None:
             move = Move()
             if self._place_bonus_heuristic is not None:
@@ -19,6 +21,9 @@ class Agent:
             if self._attach_heuristic is not None:
                 move = self._attach_heuristic(state, move)
         move.apply()
+        state.increase_turn()
+        state.increase_player_turn()
+        state.set_parent_state(current_state)
         return state
 
 
