51163d016c4614c9bb1a79567b6ee407cc9443c0
Amr Hendy <amr.m.hendy@gmail.com>
2018-11-30 01:12:22 +0200

    Added backend

--- graph/views.py
+++ graph/views.py
@@ -5,5 +5,5 @@ from django.http import JsonResponse
 # Create your views here.
 def index(request):
     #this is the shape of the request add your logic here based on that.
-    #print(request.POST.dict()['json'])
+    print(request.POST.dict())
     return render(request, 'index.html')
